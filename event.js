e9bfbecd277a0177c576ae68dd1385cb867e05a7
Maciek W <maciej.wmroblewski@gmail.com>
2017-01-09 11:53:57 +0100

    po zmianie dziala poprwnie

--- event.js
+++ event.js
@@ -4,7 +4,5 @@ var add = document.getElementById('addElem');
 
 add.addEventListener('click', function () {
 	var num = document.getElementsByTagName('li').length;
-	list.innerHTML += '<li>item</li>' + num
+	list.innerHTML += '<li>item '+ num +'</li>' 
 });
-
-// Tak ale jak tak zrobię to numer dodaje się w osobnej lini a nie obok a nie wiem czy można wrzucać w tag html zmienna
